d59f065ebc1214dafb246200cb4d9aab5fc46729
azurwastaken <caseaux.maxime@gmail.com>
2025-07-24 10:51:22 +0200

    Update mod.rs

--- books/element-of-quantitative-investing/src/portfolio/mod.rs
+++ books/element-of-quantitative-investing/src/portfolio/mod.rs
@@ -13,10 +13,22 @@ pub struct Portfolio {
 impl Portfolio {
 
     // Book p36
+    // TODO do better handling for current_prices
     pub fn compute_return(&self, current_prices: HashMap<String, Decimal>) -> Decimal {
         self.active_position.iter().map(|pos| {
+            //TODO : remove unwrap
             ((pos.amount_usd_invested() / pos.entry_price()) * current_prices.get(pos.asset()).unwrap()) - pos.amount_usd_invested()
         }).sum()
+    }
+
+    // This is to know if its worth to invest in our fund or not
+    // its basically our return vs return of safe invest (ex Livret A in France)
+    pub fn compute_excess_return(&self, current_prices: HashMap<String, Decimal>, risk_free_rate : Decimal) -> Decimal{
+        let risk_free_return: Decimal = self.active_position.iter().map(|pos| {
+            pos.amount_usd_invested() * risk_free_rate
+        }).sum();
+        let folio_return = self.compute_return(current_prices);
+        folio_return - risk_free_return
     }
 
     // TODO: for this I need historical return over days
